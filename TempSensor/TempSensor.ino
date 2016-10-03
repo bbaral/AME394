bebec0444874e361d00af25d3a7747a0f5dc2a6c
Bikram Baral <baralbikram@hotmail.com>
2016-10-03 09:00:05 -0700

    HW

--- TempSensor/TempSensor.ino
+++ TempSensor/TempSensor.ino
@@ -3,14 +3,9 @@
  * Assignment: 2 - Temperature Sensor
  */
 
-
-
-/*  Show temperature with  TMP36/LM35/2N3904 temperature sensor
-  copy right John Yu
-  connect VS1838B to  D10 see http://osoyoo.com/?p=160
-*/
+int ledPinpoint = D3;
  int tmp36sensor_pin = 5;
-//int analog_no = 5 ;// define A5 as input signal pin
+ 
 void setup()
 {
   Serial.begin(9600);
@@ -18,22 +13,25 @@ void setup()
 void loop()
 {
   int pin_value;
-  //int temperature;
   int fah;
   
-  //pin_value = analogRead(analog_no);
-  
-  //temperature = (44*pin_value)>>8 ;
+
   int reading = analogRead(tmp36sensor_pin);
   float voltage = reading * .004882814;
-  //voltage /= 1024.0;
+  voltage /= 1024.0;
+  analogWrite(ledPinpoint, voltage);
   float temperatureC = (voltage - 0.5) * 80;
+  float fah = temperatureC * 1.8 + 32;
   Serial.print("Current temperature is ");
   Serial.print(temperatureC);
-  Serial.println("C");
-  fah = temperatureC * 1.8 + 32;
+  Serial.print(" C");
+  //putting the condition for LED
+  if(fah > 70 && fah < 150) 
+  {
+    digitalWrite(fah, HIGH)
+  }
   Serial.print("Current temperature is ");
   Serial.print(fah);
-  Serial.println("F");
+  Serial.print(" F");
   delay(400);
 }
