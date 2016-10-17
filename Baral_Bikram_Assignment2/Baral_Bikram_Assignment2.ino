1987269a472f879ad36d5724674c6e5c42132cb4
Bikram Baral <baralbikram@hotmail.com>
2016-10-17 11:00:12 -0700

    Assignment2

--- Baral_Bikram_Assignment2/Baral_Bikram_Assignment2.ino
+++ Baral_Bikram_Assignment2/Baral_Bikram_Assignment2.ino
@@ -1,37 +1,113 @@
-/*
- * Name: Bikram Baral
- * Assignment: 2 - Temperature Sensor
- */
+//Bikram Baral
+//Assignment 2
 
-int ledPinpoint = D3;
- int tmp36sensor_pin = 5;
- 
-void setup()
-{
+const int pin1 = 2;
+const int pin2 = 3;
+const int pin3 = 4;
+const int pin4 = 5;
+const int pin5 = 6;
+const int pin6 = 7;
+const int pin7 = 8;
+const int pin8 = 9;
+const int pin9 = 10;
+const int pin10 = 11;
+//pin for the temperature sensor
+int sensorPin = 13;
+
+void setup() {
+
   Serial.begin(9600);
+  // make the pins outputs:
+  pinMode(pin1, OUTPUT);
+  pinMode(pin2, OUTPUT);
+  pinMode(pin3, OUTPUT);
+  pinMode(pin4, OUTPUT);
+  pinMode(pin5, OUTPUT);
+  pinMode(pin6, OUTPUT);
+  pinMode(pin7, OUTPUT);
+  pinMode(pin8, OUTPUT);
+  pinMode(pin9, OUTPUT);
+  pinMode(pin10, OUTPUT);
+  pinMode(sensorPin, INPUT);
 }
-void loop()
-{
-  int pin_value;
-  int fah;
-  
 
-  int reading = analogRead(tmp36sensor_pin);
-  float voltage = reading * .004882814;
+void loop() {
+
+  int reading = analogRead(sensorPin);
+
+  float voltage = reading * 5.0;
   voltage /= 1024.0;
-  analogWrite(ledPinpoint, voltage);
-  float temperatureC = (voltage - 0.5) * 80;
-  float fah = temperatureC * 1.8 + 32;
-  Serial.print("Current temperature is ");
-  Serial.print(temperatureC);
-  Serial.print(" C");
-  //putting the condition for LED
-  if(fah > 70 && fah < 150) 
-  {
-    digitalWrite(fah, HIGH)
+  //getting *C temperature
+  float TempC = (voltage - 0.49) * (100);
+  //converting *C to *F
+  float TempF = (TempC * 9.0 / 5.0) + 32.0;
+
+  int percent = Serial.parseInt();
+
+
+  int TemporaryValue = TempF;
+  percent = TemporaryValue;
+  Serial.println("Current Temperature " + percent);
+
+
+  while (Serial.available() > 0) {
+    if (Serial.read() == '\n')
+    {
+      if (percent > 40) {
+        digitalWrite(pin1, HIGH);
+      } else {
+        digitalWrite(pin1, LOW);
+      }
+      if (percent > 50) {
+        digitalWrite(pin2, HIGH);
+      } else {
+        digitalWrite(pin2, LOW);
+      }
+      if (percent > 60) {
+        digitalWrite(pin3, HIGH);
+      } else {
+        digitalWrite(pin3, LOW);
+      }
+      if (percent > 70) {
+        digitalWrite(pin4, HIGH);
+      } else {
+        digitalWrite(pin4, LOW);
+      }
+      if (percent > 80) {
+        digitalWrite(pin5, HIGH);
+      } else {
+        digitalWrite(pin5, LOW);
+      }
+      if (percent > 90) {
+        digitalWrite(pin6, HIGH);
+      } else {
+        digitalWrite(pin6, LOW);
+      }
+      if (percent > 100) {
+        digitalWrite(pin7, HIGH);
+      } else {
+        digitalWrite(pin7, LOW);
+      }
+      if (percent > 110) {
+        digitalWrite(pin8, HIGH);
+      } else {
+        digitalWrite(pin8, LOW);
+      }
+
+      if (percent > 125) {
+        digitalWrite(pin9, HIGH);
+      } else {
+        digitalWrite(pin9, LOW);
+      }
+
+      if (percent > 140) {
+        digitalWrite(pin10, HIGH);
+      } else {
+        digitalWrite(pin10, LOW);
+      }
+
+
+      delay(1000);
+    }
   }
-  Serial.print("Current temperature is ");
-  Serial.print(fah);
-  Serial.print(" F");
-  delay(400);
 }
